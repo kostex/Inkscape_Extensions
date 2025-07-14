b5257ed8a8372dff9dd051c96b30b1c98ab7f853
Roel Koster <koelooptiemanna@gmail.com>
2025-07-15 00:42:59 +0300

    object array better way of duplicating objects

--- ktx_object_array.py
+++ ktx_object_array.py
@@ -10,6 +10,7 @@ Lets array a bunch of objects
 import inkex
 from inkex import etree
 from inkex import transforms
+from math import floor
 
 class KTXObjectArray(inkex.EffectExtension):
     def add_arguments(self, pars):
@@ -19,6 +20,8 @@ class KTXObjectArray(inkex.EffectExtension):
         pars.add_argument("--num_y", type=int, default=4)
 
     def effect(self):
+        svg = self.document.getroot()
+
         offset_x = self.options.offset_x
         offset_y = self.options.offset_y
 
@@ -27,12 +30,15 @@ class KTXObjectArray(inkex.EffectExtension):
 
         for node in self.svg.selection:
             if node is not None:
+                pos_x = node.get('x')
+                pos_y = node.get('y')
                 for i in range(num_x):
                     for j in range(num_y):
                         if i == 0 and j == 0:
                             continue  # Skip original object
                         new_node = etree.Element(node.tag, node.attrib)
-                        new_node.set("transform", f"translate({self.svg.unittouu( str(i * offset_x) + "mm")}, {self.svg.unittouu( str(j * offset_y) + "mm")})")
+                        new_node.set("x", f"{float(pos_x)+(i * offset_x)}")
+                        new_node.set("y", f"{float(pos_y)+(j * offset_y)}")
                         node.getparent().append(new_node)
 
 if __name__ == "__main__":
